2be6fb10436cb7716f1694217d54bc7967fdb264
toutou-loves-study <shuimuyiqq@126.com>
2018-06-10 17:39:01 +0800

    nr n n
    r gnr

--- ArcEngineProgram/ArcEngineProgram/Form1.Designer.cs
+++ ArcEngineProgram/ArcEngineProgram/Form1.Designer.cs
@@ -31,6 +31,7 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -40,7 +41,7 @@
             this.button1.ForeColor = System.Drawing.SystemColors.MenuHighlight;
             this.button1.Location = new System.Drawing.Point(96, 120);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(191, 149);
+            this.button1.Size = new System.Drawing.Size(219, 149);
             this.button1.TabIndex = 0;
             this.button1.Text = "测量辅助系统";
             this.button1.UseVisualStyleBackColor = false;
@@ -53,16 +54,27 @@
             this.button2.ForeColor = System.Drawing.SystemColors.MenuHighlight;
             this.button2.Location = new System.Drawing.Point(415, 120);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(191, 149);
+            this.button2.Size = new System.Drawing.Size(223, 149);
             this.button2.TabIndex = 1;
             this.button2.Text = "建立等高线";
             this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.button3.Location = new System.Drawing.Point(563, 321);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "退出";
+            this.button3.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.BackColor = System.Drawing.Color.LightGreen;
             this.ClientSize = new System.Drawing.Size(739, 429);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.DoubleBuffered = true;
@@ -79,6 +91,7 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
